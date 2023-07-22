2582bf4e46aa39fd4a62bbaa920467a29ecd7604
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-21 18:35:48 -0700

    fixing betty code

--- sort.h
+++ sort.h
@@ -10,9 +10,12 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+#endif /** SORT_H */
