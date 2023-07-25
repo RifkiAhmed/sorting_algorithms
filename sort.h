31d882aa2d2fd96d1c8a5077a1ed5c2cb2af7314
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-25 16:40:21 -0700

    adding the prototype of Quick Sort - Hoare Partition scheme

--- sort.h
+++ sort.h
@@ -22,4 +22,5 @@ void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void cocktail_sort_list(listint_t **list);
+void quick_sort_hoare(int *array, size_t size);
 #endif /** SORT_H */
